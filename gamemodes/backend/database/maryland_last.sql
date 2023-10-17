7d258434509f5d28f6a6b55c39ffd9165a07bf22
Silent <96439992+reals1lent@users.noreply.github.com>
2023-10-17 14:02:22 +0200

    Last DB

---
+++ gamemodes/backend/database/maryland_last.sql
@@ -0,0 +1,499 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2023 at 02:01 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maryland`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bankers`
+--
+
+CREATE TABLE IF NOT EXISTS `bankers` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Skin` smallint(3) NOT NULL,
+  `PosX` float NOT NULL,
+  `PosY` float NOT NULL,
+  `PosZ` float NOT NULL,
+  `PosA` float NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `bank_accounts` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Owner` varchar(24) NOT NULL,
+  `Password` varchar(32) NOT NULL,
+  `Balance` int(11) NOT NULL,
+  `CreatedOn` int(11) NOT NULL,
+  `LastAccess` int(11) NOT NULL,
+  `Disabled` smallint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_atms`
+--
+
+CREATE TABLE IF NOT EXISTS `bank_atms` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PosX` float NOT NULL,
+  `PosY` float NOT NULL,
+  `PosZ` float NOT NULL,
+  `RotX` float NOT NULL,
+  `RotY` float NOT NULL,
+  `RotZ` float NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_logs`
+--
+
+CREATE TABLE IF NOT EXISTS `bank_logs` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `AccountID` int(11) NOT NULL,
+  `ToAccountID` int(11) NOT NULL DEFAULT -1,
+  `Type` smallint(1) NOT NULL,
+  `Player` varchar(24) NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `Date` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `bank_logs_ibfk_1` (`AccountID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `businesses`
+--
+
+CREATE TABLE IF NOT EXISTS `businesses` (
+  `bizID` int(12) NOT NULL AUTO_INCREMENT,
+  `bizName` varchar(32) DEFAULT NULL,
+  `bizOwner` int(12) DEFAULT 0,
+  `bizType` int(12) DEFAULT 0,
+  `bizPrice` int(12) DEFAULT 0,
+  `bizPosX` float DEFAULT 0,
+  `bizPosY` float DEFAULT 0,
+  `bizPosZ` float DEFAULT 0,
+  `bizPosA` float DEFAULT 0,
+  `bizIntX` float DEFAULT 0,
+  `bizIntY` float DEFAULT 0,
+  `bizIntZ` float DEFAULT 0,
+  `bizIntA` float DEFAULT 0,
+  `bizInterior` int(12) DEFAULT 0,
+  `bizExterior` int(12) DEFAULT 0,
+  `bizExteriorVW` int(12) DEFAULT 0,
+  `bizLocked` int(4) DEFAULT 0,
+  `bizVault` int(12) DEFAULT 0,
+  `bizProducts` int(12) DEFAULT 0,
+  `bizPrice1` int(12) DEFAULT 0,
+  `bizPrice2` int(12) DEFAULT 0,
+  `bizPrice3` int(12) DEFAULT 0,
+  `bizPrice4` int(12) DEFAULT 0,
+  `bizPrice5` int(12) DEFAULT 0,
+  `bizPrice6` int(12) DEFAULT 0,
+  `bizPrice7` int(12) DEFAULT 0,
+  `bizPrice8` int(12) DEFAULT 0,
+  `bizPrice9` int(12) DEFAULT 0,
+  `bizPrice10` int(12) DEFAULT 0,
+  `bizSpawnX` float DEFAULT 0,
+  `bizSpawnY` float DEFAULT 0,
+  `bizSpawnZ` float DEFAULT 0,
+  `bizSpawnA` float DEFAULT 0,
+  `bizDeliverX` float DEFAULT 0,
+  `bizDeliverY` float DEFAULT 0,
+  `bizDeliverZ` float DEFAULT 0,
+  `bizMessage` varchar(128) DEFAULT NULL,
+  `bizPrice11` int(12) DEFAULT 0,
+  `bizPrice12` int(12) DEFAULT 0,
+  `bizPrice13` int(12) DEFAULT 0,
+  `bizPrice14` int(12) DEFAULT 0,
+  `bizPrice15` int(12) DEFAULT 0,
+  `bizPrice16` int(12) DEFAULT 0,
+  `bizPrice17` int(12) DEFAULT 0,
+  `bizPrice18` int(12) DEFAULT 0,
+  `bizPrice19` int(12) DEFAULT 0,
+  `bizPrice20` int(12) DEFAULT 0,
+  `bizShipment` int(4) DEFAULT 0,
+  PRIMARY KEY (`bizID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `containers`
+--
+
+CREATE TABLE IF NOT EXISTS `containers` (
+  `conID` int(11) NOT NULL AUTO_INCREMENT,
+  `con_x` float DEFAULT 0,
+  `con_y` float DEFAULT 0,
+  `con_z` float DEFAULT 0,
+  `con_rx` float DEFAULT 0,
+  `con_ry` float DEFAULT 0,
+  `con_rz` float DEFAULT 0,
+  `con_jnumber` mediumint(9) DEFAULT 0,
+  PRIMARY KEY (`conID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faction_police`
+--
+
+CREATE TABLE IF NOT EXISTS `faction_police` (
+  `fPoliceID` int(11) NOT NULL AUTO_INCREMENT,
+  `fPoliceName` varchar(60) NOT NULL DEFAULT 'Police',
+  `fPoliceShortName` varchar(30) NOT NULL DEFAULT 'PD',
+  `fPoliceAdress` varchar(35) NOT NULL DEFAULT 'Nepoznata',
+  `fPoliceState` varchar(30) NOT NULL,
+  `fPoliceBoss` int(11) NOT NULL,
+  `fPoliceType` int(11) NOT NULL DEFAULT 0,
+  `fPoliceX` float NOT NULL DEFAULT 0,
+  `fPoliceY` float NOT NULL DEFAULT 0,
+  `fPoliceZ` float NOT NULL DEFAULT 0,
+  `fPoliceExitX` float NOT NULL DEFAULT 246.66,
+  `fPoliceExitY` float NOT NULL DEFAULT 65.8,
+  `fPoliceExitZ` float NOT NULL DEFAULT 1003.64,
+  `fPoliceInt` int(11) NOT NULL DEFAULT 6,
+  `fPoliceVault` int(11) NOT NULL DEFAULT 0,
+  `fPoliceMoney` int(11) NOT NULL DEFAULT 0,
+  `fPoliceDirtMoney` int(11) NOT NULL DEFAULT 0,
+  `fConfiscatedDrugs` int(11) NOT NULL DEFAULT 0,
+  `fDutyPointX` float NOT NULL DEFAULT 0,
+  `fDutyPointY` float NOT NULL DEFAULT 0,
+  `fDutyPointZ` float NOT NULL DEFAULT 0,
+  `fEquipmentX` float NOT NULL DEFAULT 0,
+  `fEquipmentY` float NOT NULL DEFAULT 0,
+  `fEquipmentZ` float NOT NULL DEFAULT 0,
+  `fPoliceRank1` varchar(32) NOT NULL DEFAULT 'Rank',
+  `fPoliceRank2` varchar(32) NOT NULL DEFAULT 'Rank',
+  `fPoliceRank3` varchar(32) NOT NULL DEFAULT 'Rank',
+  `fPoliceSkins1` int(11) NOT NULL DEFAULT 1,
+  `fPoliceSkins2` int(11) NOT NULL DEFAULT 1,
+  `fPoliceSkins3` int(11) NOT NULL DEFAULT 1,
+  `fPoliceSkins4` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`fPoliceID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `houses`
+--
+
+CREATE TABLE IF NOT EXISTS `houses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PID` int(11) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `Adress` varchar(50) NOT NULL,
+  `Locked` tinyint(1) NOT NULL,
+  `PosX` float NOT NULL,
+  `PosY` float NOT NULL,
+  `PosZ` float NOT NULL,
+  `ExitX` float NOT NULL,
+  `ExitY` float NOT NULL,
+  `ExitZ` float NOT NULL,
+  `Safe` tinyint(1) NOT NULL,
+  `Money` int(11) NOT NULL,
+  `Weed` int(11) NOT NULL,
+  `Cocaine` int(11) NOT NULL,
+  `Extazy` int(11) NOT NULL,
+  `WardX` float NOT NULL,
+  `WardY` float NOT NULL,
+  `WardZ` float NOT NULL,
+  `FridgeX` float NOT NULL,
+  `FridgeY` float NOT NULL,
+  `FridgeZ` float NOT NULL,
+  `Int` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `PID` (`PID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(32) NOT NULL,
+  `Salary` int(11) NOT NULL,
+  `Uniform` int(11) NOT NULL,
+  `UniformX` float NOT NULL,
+  `UniformY` float NOT NULL,
+  `UnifromZ` float NOT NULL,
+  `PositionX` float NOT NULL,
+  `PositionY` float NOT NULL,
+  `PositionZ` float NOT NULL,
+  `Interior` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klupe`
+--
+
+CREATE TABLE IF NOT EXISTS `klupe` (
+  `seat_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `seat_x` float NOT NULL,
+  `seat_y` float NOT NULL,
+  `seat_z` float NOT NULL,
+  `seat_a` float NOT NULL,
+  PRIMARY KEY (`seat_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `metros`
+--
+
+CREATE TABLE IF NOT EXISTS `metros` (
+  `metroID` int(11) NOT NULL AUTO_INCREMENT,
+  `metroX` float NOT NULL,
+  `metroY` float NOT NULL,
+  `metroZ` float NOT NULL,
+  `metroRuta` int(11) NOT NULL DEFAULT 1,
+  `metroInt` int(11) NOT NULL DEFAULT 0,
+  `metroVw` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`metroID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(25) NOT NULL DEFAULT 'Ime_Prezime',
+  `Password` int(11) NOT NULL,
+  `Level` int(11) NOT NULL DEFAULT 1,
+  `Novac` int(11) NOT NULL DEFAULT 2000,
+  `Skin` int(11) NOT NULL DEFAULT 29,
+  `Godine` int(11) NOT NULL,
+  `Staff` int(11) NOT NULL DEFAULT 0,
+  `LastLogin` varchar(50) NOT NULL DEFAULT 'NEMA',
+  `RegisterDate` varchar(50) NOT NULL DEFAULT 'NEMA',
+  `Drzava` varchar(50) NOT NULL DEFAULT 'Srbija',
+  `Pol` varchar(10) NOT NULL DEFAULT 'Nema',
+  `Email` varchar(50) NOT NULL DEFAULT '@gmail.com',
+  `Objekat0` tinyint(4) NOT NULL DEFAULT -1,
+  `Objekat1` tinyint(4) NOT NULL DEFAULT -1,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`ID`, `Username`, `Password`, `Level`, `Novac`, `Skin`, `Godine`, `Staff`, `LastLogin`, `RegisterDate`, `Drzava`, `Pol`, `Email`, `Objekat0`, `Objekat1`) VALUES
+(1, 'Silent', 297206591, 1, 4000, 250, 21, 4, '17/10/2023 - 13:57', '16/10/2023 - 18:48', 'Maryland', 'Musko', 'vostic@gmail.com', -1, -1),
+(2, 'Silent_Developer', 297206591, 1, 2000, 250, 21, 0, 'NEMA', '16/10/2023 - 18:55', 'Maryland', 'Musko', 'Vostica@gmail.com', -1, -1),
+(3, 'Silent_Developera', 297206591, 1, 2000, 250, 21, 0, 'NEMA', '16/10/2023 - 19:02', 'Maryland', 'Musko', 'Vostica@gmail.com', -1, -1),
+(4, 'Silent_Developeraa', 293602177, 1, 2000, 29, 21, 4, '16/10/2023 - 19:07', '16/10/2023 - 19:07', 'Maryland', 'Musko', 'Vostic@gmail.com', 1, -1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_crypto`
+--
+
+CREATE TABLE IF NOT EXISTS `player_crypto` (
+  `crypto_id` int(11) NOT NULL,
+  `KolicinaBTC` float DEFAULT NULL,
+  `KolicinaETH` float DEFAULT NULL,
+  `KolicinaLTC` float DEFAULT NULL,
+  `KolicinaUSDT` float DEFAULT NULL,
+  `KolicinaDOT` float DEFAULT NULL,
+  UNIQUE KEY `crypto_id` (`crypto_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `player_crypto`
+--
+
+INSERT INTO `player_crypto` (`crypto_id`, `KolicinaBTC`, `KolicinaETH`, `KolicinaLTC`, `KolicinaUSDT`, `KolicinaDOT`) VALUES
+(1, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_documents`
+--
+
+CREATE TABLE IF NOT EXISTS `player_documents` (
+  `player_id` int(11) NOT NULL,
+  `NationalID` int(11) NOT NULL,
+  `Passport` int(11) NOT NULL,
+  `VoziloLicence` int(11) NOT NULL,
+  `MotoLicence` int(11) NOT NULL,
+  `BrodLicence` int(11) NOT NULL,
+  `OruzjeLicence` int(11) NOT NULL,
+  `ZivotnoOsiguranje` tinyint(4) NOT NULL DEFAULT -1,
+  `ZivotnoTraje` datetime NOT NULL DEFAULT current_timestamp(),
+  UNIQUE KEY `player_id` (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `player_documents`
+--
+
+INSERT INTO `player_documents` (`player_id`, `NationalID`, `Passport`, `VoziloLicence`, `MotoLicence`, `BrodLicence`, `OruzjeLicence`, `ZivotnoOsiguranje`, `ZivotnoTraje`) VALUES
+(1, 0, 0, 0, 0, 0, 0, -1, '2023-10-16 18:54:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_electronic`
+--
+
+CREATE TABLE IF NOT EXISTS `player_electronic` (
+  `player_id` int(11) NOT NULL,
+  `Dron` tinyint(4) NOT NULL DEFAULT 0,
+  `Baterije` int(11) NOT NULL DEFAULT 0,
+  `Navigacija` tinyint(4) NOT NULL DEFAULT 0,
+  UNIQUE KEY `player_id` (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `player_electronic`
+--
+
+INSERT INTO `player_electronic` (`player_id`, `Dron`, `Baterije`, `Navigacija`) VALUES
+(1, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_finance`
+--
+
+CREATE TABLE IF NOT EXISTS `player_finance` (
+  `finance_id` int(11) NOT NULL,
+  `BankAccount` tinyint(4) NOT NULL DEFAULT 0,
+  `BankMoney` int(11) NOT NULL DEFAULT 0,
+  `BankPin` int(11) NOT NULL DEFAULT 0,
+  UNIQUE KEY `finance_id` (`finance_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `player_finance`
+--
+
+INSERT INTO `player_finance` (`finance_id`, `BankAccount`, `BankMoney`, `BankPin`) VALUES
+(1, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_property`
+--
+
+CREATE TABLE IF NOT EXISTS `player_property` (
+  `player_id` int(11) NOT NULL,
+  `HouseID` int(11) NOT NULL,
+  UNIQUE KEY `player_id` (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `player_property`
+--
+
+INSERT INTO `player_property` (`player_id`, `HouseID`) VALUES
+(1, -1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `safezones`
+--
+
+CREATE TABLE IF NOT EXISTS `safezones` (
+  `safeSQLID` int(11) NOT NULL AUTO_INCREMENT,
+  `MinX` float NOT NULL DEFAULT 0,
+  `MinY` float NOT NULL DEFAULT 0,
+  `MaxX` float NOT NULL DEFAULT 0,
+  `MaxY` float NOT NULL DEFAULT 0,
+  `Radius` float NOT NULL DEFAULT 0,
+  `Color` int(11) NOT NULL DEFAULT 0,
+  `PickupX` float NOT NULL DEFAULT 0,
+  `PickupY` float NOT NULL DEFAULT 0,
+  `PickupZ` float NOT NULL DEFAULT 0,
+  PRIMARY KEY (`safeSQLID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE IF NOT EXISTS `vehicles` (
+  `vID` int(11) NOT NULL AUTO_INCREMENT,
+  `vOwner` int(11) NOT NULL DEFAULT 0,
+  `vModel` int(11) NOT NULL DEFAULT 0,
+  `Color1` int(11) NOT NULL DEFAULT 0,
+  `Color2` int(11) NOT NULL DEFAULT 0,
+  `vPlate` varchar(32) NOT NULL DEFAULT 'UNREGISTERED-00',
+  `vPosX` float NOT NULL,
+  `vPosY` float NOT NULL,
+  `vPosZ` float NOT NULL,
+  `vPosA` float NOT NULL,
+  `vRegDate` int(11) NOT NULL DEFAULT 0,
+  `vOil` int(11) NOT NULL DEFAULT 100,
+  `vRange` int(11) NOT NULL DEFAULT 0,
+  `vRangeKM` int(11) NOT NULL DEFAULT 0,
+  `vFuel` int(11) NOT NULL DEFAULT 100,
+  `vFuelType` int(11) NOT NULL DEFAULT 1,
+  `vAlarm` tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`vID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`vID`, `vOwner`, `vModel`, `Color1`, `Color2`, `vPlate`, `vPosX`, `vPosY`, `vPosZ`, `vPosA`, `vRegDate`, `vOil`, `vRange`, `vRangeKM`, `vFuel`, `vFuelType`, `vAlarm`) VALUES
+(1, 0, 562, 1, 1, 'N/A-00-12', 816.957, -1327.21, 13.4923, 32.244, 0, 100, 0, 0, 100, 1, 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
